d815b2e52206f1587c5dfbdf29782ccc268828f9
ChauveSouris <desseobscurite@gmail.com>
2023-05-08 23:01:17 +0500

    Initial Commit

---
+++ StackView(M)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackView(M)
+//
+//  Created by Елизавета Медведева on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
