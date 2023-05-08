bab78c158b0773d347a22042bdac5f5394dba649
ChauveSouris <desseobscurite@gmail.com>
2023-05-09 00:24:38 +0500

    Update ViewController.swift
    Connected Start Screen with ViewController

--- StackView(M)/ViewController.swift
+++ StackView(M)/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    
+    @IBOutlet var changeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func changeColorButtonDidTapped() {
+        
+    }
+    
 
 }
 
