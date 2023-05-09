9dbc5870bc5a2bc2b695c4dea39338a8e6afad96
ChauveSouris <desseobscurite@gmail.com>
2023-05-10 00:38:40 +0500

    Create func for change color

--- StackView(M)/ViewController.swift
+++ StackView(M)/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
     @IBOutlet var redColorView: UIView!
     @IBOutlet var yellowColorView: UIView!
     @IBOutlet var greenColorView: UIView!
-    
-    
     @IBOutlet var changeColorButton: UIButton!
+    
+    let colorViews = ["allColor 0.3", "red 1.0", "yellow 1.0", "green 1.0"]
+    var currentIColorIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateColor()
+        
         redColorView.layer.cornerRadius = 50
         yellowColorView.layer.cornerRadius = 50
         greenColorView.layer.cornerRadius = 50
@@ -28,14 +32,35 @@ class ViewController: UIViewController {
         greenColorView.alpha = 0.3
        
     }
-
+    
+    func updateColor(){
+        let colorView = colorViews[currentIColorIndex]
+        
+        switch colorView {
+        case "red 1.0":
+            redColorView.alpha = 1.0
+        case "yellow 1.0":
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1.0
+        case "green 1.0":
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 1.0
+        default:
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 0.3
+            changeColorButton.setTitle("START", for: .normal)
+        }
+    }
     
     @IBAction func changeColorButtonDidTapped() {
-        redColorView.alpha = 1.0
         changeColorButton.setTitle("NEXT", for: .normal)
         
-        
-        
+        currentIColorIndex += 1
+        if currentIColorIndex >= colorViews.count {
+            currentIColorIndex = 0
+        }
+        updateColor()
     } 
     
 
